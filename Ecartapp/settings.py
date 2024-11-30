70a48b7e4fefcb04af4d5cee1389ca6a1e5a7760
ijjiganirakesh@gmail.com <ijjiganirakesh@gmail.com>
2024-11-30 22:06:50 +0530

    some changes in view

--- Ecartapp/settings.py
+++ Ecartapp/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Ecartapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'template'],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS=[STATIC_DIR]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return HttpResponse("hello world")
